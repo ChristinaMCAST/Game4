b259ed983463732fc122664012e571e5e91ce3e4
TheGerMCAST <gerards@M112-T.mcast.org.mt>
2015-04-15 10:50:04 +0200

    Lesson 15-04-2015
    Changes in the lesson on the 15th including mouse positions and cube
    positions

---
+++ Assets/cubeController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//rotate in the Z axis
+		transform.Rotate (Vector3.forward * 10f * Time.deltaTime);
+
+		transform.Rotate (Vector3.left * 10f * Time.deltaTime);
+
+
+				if (transform.position.z > 0)
+						transform.Translate (Vector3.forward * -10f * Time.deltaTime, Space.World);
+				else
+						transform.position = new Vector3 (0f, 0f, 10f);
+
+
+	
+	}
+}
---
+++ Assets/mousecubeController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+//for the text elements
+using UnityEngine.UI;
+using System.Collections;
+
+public class mousecubeController : MonoBehaviour {
+
+
+	//1. add a reference for the text fields
+	public Text mousePosText;
+	public Text cubePosText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//2. read the mouse x and y position
+		mousePosText.text = "M X:" + Input.mousePosition.x + " Y:" + Input.mousePosition.y;
+
+		//3. translate the mouse coordinates to viewport coordinates
+		Vector3 mouseCubePos = Camera.main.ScreenToViewportPoint (Input.mousePosition);
+
+		//4. fix the Z to 10
+		mouseCubePos = new Vector3 (mouseCubePos.x, mouseCubePos.y, 10f);
+
+
+		//5. set the cube x,y position text
+		cubePosText.text = "C X:" + mouseCubePos.x + " Y:" + mouseCubePos.y;
+
+
+		//6. set the mouse cube position
+		transform.position = mouseCubePos;
+
+	}
+}
